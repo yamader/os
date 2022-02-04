c993886ef70a572a20dfa3d797780c4b6c4e186a
YamaD <me@dyama.net>
2022-02-04 23:53:00 +0900

    update loader efi library

--- src/loader/efi.d
+++ src/loader/efi.d
@@ -2,36 +2,233 @@ module efi;
 
 extern(C):
 
-alias EFI_STATUS = ulong;
-alias EFI_HANDLE = void*;
-alias EFI_TEXT_STRING = EFI_STATUS function(EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This,
-                                            wchar* String);
+// Data Types
 
-struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-  byte[8] Reset;
-  EFI_TEXT_STRING OutputString;
-  byte[8] TestString;
-  byte[8] QueryMode;
-  byte[8] SetMode;
-  byte[8] SetAttribute;
-  byte[8] ClearScreen;
-  byte[8] SetCursorPosition;
-  byte[8] EnableCursor;
-  byte[8] Mode;
+// only 64bit
+alias INTN  = long;
+alias UINTN = ulong;
+//
+alias EFI_STATUS = UINTN;
+alias EFI_HANDLE = void*;
+alias EFI_EVENT = void*;
+alias EFI_LBA = ulong;
+alias EFI_TPL = UINTN;
+
+enum Status {
+  EFU_SUCCESS,
 }
+
+// Macros
+
+bool EFI_ERROR(EFI_STATUS status) {
+  return cast(INTN)(status) < 0;
+}
+
+// EFI System Table
+
+alias EFI_TABLE_HEADER = byte[24];
 
 struct EFI_SYSTEM_TABLE {
-  byte[24] Hdr;
-  byte[8] FirmwareVendor;
-  byte[4] FirmwareRevision;
-  byte[8] ConsoleInHandle;
-  byte[8] ConIn;
-  byte[8] ConsoleOutHandle;
+  EFI_TABLE_HEADER Hdr;
+  wchar* FirmwareVendor;
+  uint FirmwareRevision;
+  EFI_HANDLE ConsoleInHandle;
+  void* buf4;
+  EFI_HANDLE ConsoleOutHandle;
   EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* ConOut;
-  byte[8] StandardErrorHandle;
-  byte[8] StdErr;
-  byte[8] RuntimeServices;
-  byte[8] BootServices;
-  byte[8] NumberOfTableEntries;
-  byte[8] ConfigurationTable;
+  EFI_HANDLE StandardErrorHandle;
+  void* buf8;
+  EFI_RUNTIME_SERVICES* RuntimeServices;
+  EFI_BOOT_SERVICES* BootServices;
+  UINTN NumberOfTableEntries;
+  void* buf12;
 }
+struct EFI_BOOT_SERVICES {
+  EFI_TABLE_HEADER Hdr;
+  void* buf1,buf2;
+  EFI_ALLOCATE_PAGES AllocatePages;
+  EFI_FREE_PAGES FreePages;
+  EFI_GET_MEMORY_MAP GetMemoryMap;
+  EFI_ALLOCATE_POOL AllocatePool;
+  EFI_FREE_POOL FreePool;
+  void* buf8,buf9,buf10,buf11,buf12,buf13,buf14,buf15,buf16,buf17,buf18,buf19,buf20,buf21;
+  EFI_IMAGE_LOAD LoadImage;
+  EFI_IMAGE_START StartImage;
+  EFI_EXIT Exit;
+  EFI_IMAGE_UNLOAD UnloadImage;
+  EFI_EXIT_BOOT_SERVICES ExitBootServices;
+  void* buf27,buf28,buf29,buf30,buf31,buf32,buf33,buf34,buf35,buf36,buf37,buf38,buf39,buf40;
+  EFI_COPY_MEM CopyMem;
+  EFI_SET_MEM SetMem;
+  void* buf43;
+}
+struct EFI_RUNTIME_SERVICES {
+  EFI_TABLE_HEADER Hdr;
+  void* buf1,buf2,buf3,buf4,buf5,buf6,buf7,buf8,buf9,buf10;
+  EFI_RESET_SYSTEM ResetSystem;
+  void* buf12,buf13,buf14;
+}
+
+// Boot Services
+
+alias EFI_PHYSICAL_ADDRESS = ulong;
+
+enum EFI_ALLOCATE_TYPE {
+  AllocateAnyPages,
+  AllocateMaxAddress,
+  AllocateAddress,
+  MaxAllocateType
+}
+enum EFI_MEMORY_TYPE {
+  EfiReservedMemoryType,
+  EfiLoaderCode,
+  EfiLoaderData,
+  EfiBootServicesCode,
+  EfiBootServicesData,
+  EfiRuntimeServicesCode,
+  EfiRuntimeServicesData,
+  EfiConventionalMemory,
+  EfiUnusableMemory,
+  EfiACPIReclaimMemory,
+  EfiACPIMemoryNVS,
+  EfiMemoryMappedIO,
+  EfiMemoryMappedIOPortSpace,
+  EfiPalCode,
+  EfiPersistentMemory,
+  EfiUnacceptedMemoryType,
+  EfiMaxMemoryType
+}
+
+struct EFI_MEMORY_DESCRIPTOR {
+  uint Type;
+  EFI_PHYSICAL_ADDRESS PhysicalStart;
+  ulong NumberOfPages;
+  ulong Attributes;
+}
+
+alias EFI_ALLOCATE_PAGES = EFI_STATUS function(
+  EFI_ALLOCATE_TYPE Type,
+  EFI_MEMORY_TYPE MemoryType,
+  UINTN Pages,
+  EFI_PHYSICAL_ADDRESS* Memory);
+alias EFI_FREE_PAGES = EFI_STATUS function(
+  EFI_PHYSICAL_ADDRESS Memory,
+  UINTN Pages);
+alias EFI_GET_MEMORY_MAP = EFI_STATUS function(
+  UINTN* MemoryMapSize,
+  EFI_MEMORY_DESCRIPTOR* MemoryMap,
+  UINTN* MapKey,
+  UINTN* DescriptorSize,
+  uint* DescriptorVersion);
+alias EFI_ALLOCATE_POOL = EFI_STATUS function(
+  EFI_MEMORY_TYPE PoolType,
+  UINTN Size,
+  void** Buffer);
+alias EFI_FREE_POOL = EFI_STATUS function(
+  void* Buffer);
+alias EFI_IMAGE_LOAD = EFI_STATUS function(
+  bool BootPilicy,
+  EFI_HANDLE ParentImageHandle,
+  EFI_DEVICE_PATH_PROTOCOL* DevicePath,
+  void* SourceBuffer,
+  UINTN SourceSize,
+  EFI_HANDLE* ImageHandle);
+alias EFI_IMAGE_START = EFI_STATUS function(
+  EFI_HANDLE ImageHandle,
+  UINTN* ExitDataSize,
+  wchar** ExitData);
+alias EFI_IMAGE_UNLOAD = EFI_STATUS function(
+  EFI_HANDLE ImageHandle);
+alias EFI_IMAGE_ENTRY_POINT = EFI_STATUS function(
+  EFI_HANDLE ImageHandle,
+  EFI_SYSTEM_TABLE* SystemTable);
+alias EFI_EXIT = EFI_STATUS function(
+  EFI_HANDLE ImageHandle,
+  EFI_STATUS ExitStatus,
+  UINTN ExitDataSize,
+  wchar* ExitData);
+alias EFI_EXIT_BOOT_SERVICES = EFI_STATUS function(
+  EFI_HANDLE ImageHandle,
+  UINTN MapKey);
+alias EFI_COPY_MEM = void function(
+  void* Destination,
+  void* Source,
+  UINTN Length);
+alias EFI_SET_MEM = void function(
+  void* Buffer,
+  UINTN Size,
+  ubyte Value);
+
+// Runtime Services
+
+enum EFI_RESET_TYPE {
+  EfiResetCold,
+  EfiResetWarm,
+  EfiResetShutdown,
+  EfiResetPlatformSpecific
+}
+
+alias EFI_RESET_SYSTEM = void function(
+  EFI_RESET_TYPE ResetType,
+  EFI_STATUS ResetStatus,
+  UINTN DataSize,
+  void* ResetData);
+
+// Device Path Protocol
+
+struct EFI_DEVICE_PATH_PROTOCOL {
+  ubyte Type;
+  ubyte SubType;
+  ubyte[2] Length;
+}
+
+// Console Support
+
+struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL {
+  EFI_INPUT_RESET Reset;
+  EFI_INPUT_READ_KEY ReadKeyStroke;
+  EFI_EVENT WaitForKey;
+}
+struct EFI_INPUT_KEY {
+  ushort ScanCode;
+  wchar UnicodeChar;
+}
+struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
+  EFI_TEXT_RESET Reset;
+  EFI_TEXT_STRING OutputString;
+  void* buf2,buf3,buf4,buf5;
+  EFI_TEXT_CLEAR_SCREEN ClearScreen;
+  EFI_TEXT_SET_CURSOR_POSITION SetCursorPosition;
+  EFI_TEXT_ENABLE_CURSOR EnableCursor;
+  SIMPLE_TEXT_OUTPUT_MODE* Mode;
+}
+struct SIMPLE_TEXT_OUTPUT_MODE {
+  int MaxMode;
+  int Mode;
+  int Attribute;
+  int CursorColumn;
+  int CursorRow;
+  bool CursorVisible;
+}
+
+alias EFI_INPUT_RESET = EFI_STATUS function(
+  EFI_SIMPLE_TEXT_INPUT_PROTOCOL* This,
+  bool ExtendedVerfication);
+alias EFI_INPUT_READ_KEY = EFI_STATUS function(
+  EFI_SIMPLE_TEXT_INPUT_PROTOCOL* This,
+  EFI_INPUT_KEY* Key);
+alias EFI_TEXT_RESET = EFI_STATUS function(
+  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This,
+  bool ExtendedVerfication);
+alias EFI_TEXT_STRING = EFI_STATUS function(
+  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This,
+  wchar* String);
+alias EFI_TEXT_CLEAR_SCREEN = EFI_STATUS function(
+  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This);
+alias EFI_TEXT_SET_CURSOR_POSITION = EFI_STATUS function(
+  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This,
+  UINTN Column,
+  UINTN Row);
+alias EFI_TEXT_ENABLE_CURSOR = EFI_STATUS function(
+  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This,
+  bool Visible);
--- src/loader/main.d
+++ src/loader/main.d
@@ -3,7 +3,21 @@ import efi;
 
 extern(C):
 
+EFI_SYSTEM_TABLE* gST;
+EFI_BOOT_SERVICES* gBS;
+EFI_RUNTIME_SERVICES* gRT;
+
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
-  SystemTable.ConOut.OutputString(SystemTable.ConOut, cast(wchar*)"hello, world (from Dlang)"w);
+  EFI_STATUS status;
+
+  gST = SystemTable;
+  gBS = gST.BootServices;
+  gRT = gST.RuntimeServices;
+
+  gST.ConOut.OutputString(gST.ConOut, cast(wchar*)"hello, world (from Dlang)"w);
+  if(EFI_ERROR(status)) {
+    return status;
+  }
+
   while(true) asm { hlt; }
 }
