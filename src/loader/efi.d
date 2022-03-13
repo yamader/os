c64c46c26ac23fe3606adcc080222ba1da2db64c
YamaD <me@dyama.net>
2022-03-13 23:24:32 +0900

    ファイル周りのprotocolの実装

--- src/loader/efi.d
+++ src/loader/efi.d
@@ -1,6 +1,7 @@
 module loader.efi;
 
 extern(C):
+__gshared:
 
 // Data Types
 
@@ -12,6 +13,15 @@ alias EFI_HANDLE = void*;
 alias EFI_EVENT = void*;
 alias EFI_LBA = ulong;
 alias EFI_TPL = UINTN;
+
+align(64)
+struct EFI_GUID {
+  uint buf0;
+  ushort buf1,buf2;
+  struct _ {
+    ubyte buf0,buf1,buf2,buf3,buf4,buf5,buf6,buf7;
+  } _ buf3;
+}
 
 enum EFI_STATUS : UINTN {
   EFI_SUCCESS = 0uL,
@@ -97,7 +107,10 @@ struct EFI_BOOT_SERVICES {
   EFI_EXIT Exit;
   EFI_IMAGE_UNLOAD UnloadImage;
   EFI_EXIT_BOOT_SERVICES ExitBootServices;
-  void* buf27,buf28,buf29,buf30,buf31,buf32,buf33,buf34,buf35,buf36,buf37,buf38,buf39,buf40;
+  void* buf27,buf28,buf29,buf30,buf31;
+  EFI_OPEN_PROTOCOL OpenProtocol;
+  EFI_CLOSE_PROTOCOL CloseProtocol;
+  void* buf34,buf35,buf36,buf37,buf38,buf39,buf40;
   EFI_COPY_MEM CopyMem;
   EFI_SET_MEM SetMem;
   void* buf43;
@@ -139,6 +152,15 @@ enum EFI_MEMORY_TYPE {
   EfiPersistentMemory,
   EfiUnacceptedMemoryType,
   EfiMaxMemoryType
+}
+
+enum EFI_OPEN_PROTOCOL_ATTRIBUTES : uint {
+  BY_HANDLE_PROTOCOL = 0x00000001,
+  GET_PROTOCOL = 0x00000002,
+  TEST_PROTOCOL = 0x00000004,
+  BY_CHILD_CONTROLLER = 0x00000008,
+  BY_DRIVER = 0x00000010,
+  EXCLUSIVE = 0x00000020,
 }
 
 struct EFI_MEMORY_DESCRIPTOR {
@@ -199,6 +221,20 @@ alias EFI_EXIT_BOOT_SERVICES = EFI_STATUS function(
   EFI_HANDLE ImageHandle,
   UINTN MapKey);
 
+alias EFI_OPEN_PROTOCOL = EFI_STATUS function(
+  EFI_HANDLE Handle,
+  EFI_GUID* Protocol,
+  void** Interface,
+  EFI_HANDLE AgentHandle,
+  EFI_HANDLE ControllerHandle,
+  EFI_OPEN_PROTOCOL_ATTRIBUTES Attributes);
+
+alias EFI_CLOSE_PROTOCOL = EFI_STATUS function(
+  EFI_HANDLE Handle,
+  EFI_GUID* Protocol,
+  EFI_HANDLE AgentHandle,
+  EFI_HANDLE ControllerHandle);
+
 alias EFI_COPY_MEM = void function(
   void* Destination,
   void* Source,
@@ -223,6 +259,27 @@ alias EFI_RESET_SYSTEM = void function(
   EFI_STATUS ResetStatus,
   UINTN DataSize,
   void* ResetData);
+
+// EFI Loaded Image
+
+EFI_GUID gEfiLoadedImageProtocolGuid =
+  {0x5b1b31a1,0x9562,0x11d2,{0x8e,0x3f,0x00,0xa0,0xc9,0x69,0x72,0x3b}};
+
+struct EFI_LOADED_IMAGE_PROTOCOL {
+  uint Revision;
+  EFI_HANDLE ParentHandle;
+  EFI_SYSTEM_TABLE* SystemTable;
+  EFI_HANDLE DeviceHandle;
+  EFI_DEVICE_PATH_PROTOCOL* FilePath;
+  void* buf5;
+  uint LoadOptionsSize;
+  void* LoadOptions;
+  void* ImageBase;
+  ulong ImageSize;
+  EFI_MEMORY_TYPE ImageCodeType;
+  EFI_MEMORY_TYPE ImageDataType;
+  EFI_IMAGE_UNLOAD Unload;
+}
 
 // Device Path Protocol
 
@@ -291,3 +348,87 @@ alias EFI_TEXT_SET_CURSOR_POSITION = EFI_STATUS function(
 alias EFI_TEXT_ENABLE_CURSOR = EFI_STATUS function(
   EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This,
   bool Visible);
+
+// Simple File System Protocol
+
+struct EFI_SIMPLE_FILE_SYSTEM_PROTOCOL {
+  ulong Revision;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_OPEN_VOLUME OpenVolume;
+}
+
+alias EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_OPEN_VOLUME = EFI_STATUS function(
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* This,
+  EFI_FILE_PROTOCOL** Root);
+
+// File Protocol
+
+EFI_GUID gEfiSimpleFileSystemProtocolGuid =
+  {0x0964e5b22,0x6459,0x11d2,{0x8e,0x39,0x00,0xa0,0xc9,0x69,0x72,0x3b}};
+
+enum EFI_FILE_OPEN_MODE : ulong {
+  READ = 0x0000000000000001,
+  WRITE = 0x0000000000000002,
+  CREATE = 0x8000000000000000,
+}
+
+enum EFI_FILE_OPEN_ATTRIBUTES : ulong {
+  READ_ONLY = 0x0000000000000001,
+  HIDDEN = 0x0000000000000002,
+  SYSTEM = 0x0000000000000004,
+  RESERVED = 0x0000000000000008,
+  DIRECTORY = 0x0000000000000010,
+  ARCHIVE = 0x0000000000000020,
+  VALID_ATTR = 0x0000000000000037,
+}
+
+struct EFI_FILE_PROTOCOL {
+  ulong Revision;
+  EFI_FILE_OPEN Open;
+  EFI_FILE_CLOSE Close;
+  EFI_FILE_DELETE Delete;
+  EFI_FILE_READ Read;
+  EFI_FILE_WRITE Write;
+  void* buf6,buf7;
+  EFI_FILE_GET_INFO GetInfo;
+  EFI_FILE_SET_INFO SetInfo;
+  EFI_FILE_FLUSH Flush;
+  void* buf11,buf12,buf13,buf14;
+}
+
+alias EFI_FILE_OPEN = EFI_STATUS function(
+  EFI_FILE_PROTOCOL* This,
+  EFI_FILE_PROTOCOL** NewHandle,
+  wchar* FileName,
+  EFI_FILE_OPEN_MODE OpenMode,
+  EFI_FILE_OPEN_ATTRIBUTES Attributes);
+
+alias EFI_FILE_CLOSE = EFI_STATUS function(
+  EFI_FILE_PROTOCOL* This);
+
+alias EFI_FILE_DELETE = EFI_STATUS function(
+  EFI_FILE_PROTOCOL* This);
+
+alias EFI_FILE_READ = EFI_STATUS function(
+  EFI_FILE_PROTOCOL* This,
+  UINTN* BufferSize,
+  void* Buffer);
+
+alias EFI_FILE_WRITE = EFI_STATUS function(
+  EFI_FILE_PROTOCOL* This,
+  UINTN* BufferSize,
+  void* Buffer);
+
+alias EFI_FILE_GET_INFO = EFI_STATUS function(
+  EFI_FILE_PROTOCOL* This,
+  EFI_GUID* InformationType,
+  UINTN* BufferSize,
+  void* Buffer);
+
+alias EFI_FILE_SET_INFO = EFI_STATUS function(
+  EFI_FILE_PROTOCOL* This,
+  EFI_GUID* InformationType,
+  UINTN BufferSize,
+  void* Buffer);
+
+alias EFI_FILE_FLUSH = EFI_STATUS function(
+  EFI_FILE_PROTOCOL* This);
--- src/loader/main.d
+++ src/loader/main.d
@@ -28,19 +28,61 @@ EFI_STATUS Print(T...)(wstring fmt, T args) {
   }
 }
 
+EFI_STATUS OpenRootDir(EFI_HANDLE ImageHandle, EFI_FILE_PROTOCOL** root) {
+  EFI_STATUS status;
+
+  EFI_LOADED_IMAGE_PROTOCOL* img;
+  status = gBS.OpenProtocol(
+    ImageHandle,
+    &gEfiLoadedImageProtocolGuid,
+    cast(void**)&img,
+    ImageHandle,
+    null,
+    EFI_OPEN_PROTOCOL_ATTRIBUTES.BY_HANDLE_PROTOCOL);
+  if(EFI_ERROR(status)) {
+    Print("!! Error opening Loaded Image Protocol : 0x%x\r\n"w, status);
+    return status;
+  }
+
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
+  status = gBS.OpenProtocol(
+    img.DeviceHandle,
+    &gEfiSimpleFileSystemProtocolGuid,
+    cast(void**)&fs,
+    ImageHandle,
+    null,
+    EFI_OPEN_PROTOCOL_ATTRIBUTES.BY_HANDLE_PROTOCOL);
+  if(EFI_ERROR(status)) {
+    Print("!! Error opening Simple File System Protocol : 0x%x\r\n"w, status);
+    return status;
+  }
+
+  return fs.OpenVolume(fs, root);
+}
+
 EFI_STATUS UefiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
-  EFI_STATUS status = void;
+  EFI_STATUS status;
 
   gST = SystemTable;
   gBS = gST.BootServices;
   gRT = gST.RuntimeServices;
 
+  // hello, world
   gST.ConOut.Reset(gST.ConOut, false);
-  status = Print("hello, world (from Dlang)\r\n"w);
+  status = Print("Loading yamadOS ...\r\n"w);
   if(EFI_ERROR(status)) {
     return status;
   }
 
+  // load kernel file
+  EFI_FILE_PROTOCOL* root_dir;
+  status = OpenRootDir(ImageHandle, &root_dir);
+  if(EFI_ERROR(status)) {
+    Print("!! Error opening root dir\r\n");
+    return status;
+  }
+
+  // catch
   Halt();
   return EFI_STATUS.EFI_SUCCESS;
 }
