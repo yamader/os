98d87d3f92997d70bf4633efb2e5d12330be0769
YamaD <me@dyama.net>
2022-04-16 09:32:02 +0900

    memsetの実装

--- src/kernel/support.d
+++ src/kernel/support.d
@@ -1,4 +1,11 @@
 extern(C):
 
-void memset() {}
+version(assert)
 void __assert() {}
+
+void* memset(void* buf, int val, size_t len) {
+  auto s = cast(ubyte*)buf;
+  while(len--)
+    *s++ = cast(ubyte)val;
+  return buf;
+}
--- src/loader/support.d
+++ src/loader/support.d
@@ -1,4 +1,10 @@
 extern(C):
 
-void memset() {}
 void __chkstk() {}
+
+void* memset(void* buf, int val, size_t len) {
+  auto s = cast(ubyte*)buf;
+  while(len--)
+    *s++ = cast(ubyte)val;
+  return buf;
+}
